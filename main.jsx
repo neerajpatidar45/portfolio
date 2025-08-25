0c5a59d57fa1d3d9ba532de9c74f27ae0ccd6693
Neeraj Patidar <np3618560@gmail.com>
2025-08-25 15:52:32 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,102 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import React from "react";
-import { useState } from "react";
-
-import "./index.css"
-
-import App from './portfolio/App'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom';
 
-// import "./planindex.css"
-// import Planwithlove from "./Planwithlove";
-
-//import App from './App'
-//import Indecrement from './Indecrement';
-
-//import './courseindex.css'
-//import Topcourse from './Topcourse';
-//import { ToastContainer } from "react-toastify";
-//import "react-toastify/dist/ReactToastify.css";
-
-// import Testimonial from "./Testimonial"
-
-//import Formdemo from './Formdemo';
-
-//import { BrowserRouter } from 'react-router-dom';
-//import Reactrouters from './Reactrouters';
-
-//import Studynotion from "./Studynotion"
-//import { ToastContainer } from 'react-toastify';
-
-// import Randomgif from './Randomgif';
-// import "./randomgif.css"
-
-//import Blogcontext from './Blogcontext';
-//import Appcontextprovider from './blogcontexts/context/Appcontext';
-//import { BrowserRouter } from 'react-router-dom';
-
-// import Shoppingcard from './Shoppingcard';
-// import { Provider } from 'react-redux';
-// //import { storeexample } from './redux/slices/storeexample';
-// import { store } from './redux/store';
-// import { BrowserRouter } from 'react-router-dom';
-// import { ToastContainer } from 'react-toastify'
-
-
 createRoot(document.getElementById('root')).render(
-
   <BrowserRouter>
     <App/>
   </BrowserRouter>
-
-  //  <StrictMode>
-  // <App />
-
-    // <Indecrement />
-
-  // <Planwithlove />
-
-  // <div>
-  //   <Topcourse />
-  //   <ToastContainer />
-  // </div>
-
-  // <Topcourse/>
-
-   // <Testimonial/>
-  
-  //<Formdemo/>
-
-  // <BrowserRouter>
-  //   <Reactrouters/>      
-  // </BrowserRouter>
-
-  // <BrowserRouter>
-  //   <Studynotion/>
-  //   <ToastContainer/>
-  // </BrowserRouter>
-
-  //<Randomgif/>
-
-  //  <BrowserRouter>
-  //     <Appcontextprovider>
-  //         <Blogcontext/>
-  //     </Appcontextprovider>
-  //  </BrowserRouter> 
-
-  // <Provider store={storeexample}>
-
-  //   <Shoppingcard/>
-  // </Provider>
-
-  // <BrowserRouter>
-  //   <Provider store={store}>
-  //     <Shoppingcard/>
-  //     <ToastContainer/>
-  //   </Provider>
-  // </BrowserRouter>
-  
-  //  </StrictMode>,
 );
  
